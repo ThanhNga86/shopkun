fd73f53de448793a73538a6bfdf84977a63aaf0e
Thanh Ngà <testtest86n@gmail.com>
2023-11-21 18:19:24 +0700

    file

--- src/main/java/com/shopkun/error/ErrorConfiguration.java
+++ src/main/java/com/shopkun/error/ErrorConfiguration.java
@@ -13,11 +13,11 @@ public class ErrorConfiguration implements ErrorPageRegistrar {
 
 	@Override
 	public void registerErrorPages(ErrorPageRegistry registry) {
-		ErrorPage error404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error404");
-		ErrorPage error405 = new ErrorPage(HttpStatus.METHOD_NOT_ALLOWED, "/error404");
-		ErrorPage error400 = new ErrorPage(HttpStatus.BAD_REQUEST, "/error404");
-		ErrorPage error500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error404");
-		registry.addErrorPages(error404, error405, error400, error500);
+//		ErrorPage error404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error404");
+//		ErrorPage error405 = new ErrorPage(HttpStatus.METHOD_NOT_ALLOWED, "/error404");
+//		ErrorPage error400 = new ErrorPage(HttpStatus.BAD_REQUEST, "/error404");
+//		ErrorPage error500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error404");
+//		registry.addErrorPages(error404, error405, error400, error500);
 	}
 
 }
